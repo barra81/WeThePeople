7b317ca040aee7d31b0cefa94636f02bfda737b8
Erik Sandbraaten <erik.sandbraaten@gmail.com>
2025-02-13 17:12:27 +0100

    Fix crash in CvUnit::getTriggerValue caused by a unit having a NULL plot when the parameter bCheckPlot is set

--- Project Files/DLLSources/CvUnit.cpp
+++ Project Files/DLLSources/CvUnit.cpp
@@ -14431,7 +14431,7 @@ int CvUnit::getTriggerValue(EventTriggerTypes eTrigger, const CvPlot* pPlot, boo
 		}
 	}
 
-	if (bCheckPlot)
+	if (bCheckPlot && pPlot != NULL)
 	{
 		if (kTrigger.isUnitsOnPlot())
 		{
